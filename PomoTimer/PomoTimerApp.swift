b9efe1dca46c0a925a53cfaa295c79655e377007
Sravan Karuturi <sravankumarkaruturi@gmail.com>
2023-09-19 21:17:51 -0700

    Initial Commit

---
+++ PomoTimer/PomoTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoTimerApp.swift
+//  PomoTimer
+//
+//  Created by Sravan Karuturi on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct PomoTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
