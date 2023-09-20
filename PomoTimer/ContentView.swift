1092e6ed003900478f2aa2223f1d2acf4b6361f4
Sravan Karuturi <sravankumarkaruturi@gmail.com>
2023-09-19 21:45:34 -0700

    Initial Commit

--- PomoTimer/ContentView.swift
+++ PomoTimer/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var endTime: Date = Date(timeInterval: 1500, since: Date())
+    
+    private var formatter = DateComponentsFormatter()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (spacing: 50) {
+            Text("Time Left")
+                .font(.subheadline)
+            
+            let currTime = Date()
+            let timeInt = currTime.distance(to: endTime)
+            
+            Text(formatter.string(from: timeInt)!)
+            
+            Button(action: ResetTimer, label: {Text("Reset Timer")})
+            
         }
         .padding()
+    }
+    
+    func ResetTimer(){
+        endTime = Date(timeInterval: 1500, since: Date())
     }
 }
 
